64da80461370abc3fd0ca831531e77f3ad1732c9
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-18 15:03:05 -0600

    removed styled components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components';
+
 
 export default function Login() {
     const handleClick = () => {
@@ -22,32 +22,10 @@ export default function Login() {
     }
 
   return (
-    <Container>
+    <div>
         <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="spotify" />
         <button onClick={handleClick}>Connect to Spotify</button>
-    </Container>
+    </div>
   )
 }
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: #1db954;
-    gap: 5rem;
-    img {
-        height: 20vh;
-    }
-    button {
-        padding: 1rem 5rem;
-        border-radius: 5rem;
-        border: none;
-        background-color: black;
-        color: #49f585;
-        font-size: 1.4rem;
-        cursor: pointer;
-    }
-`;
