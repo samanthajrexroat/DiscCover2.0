f4d687f9249f1779c081049f9aa987349d1ee9c9
Ccatalyst <unclejunglegeorge@gmail.com>
2022-05-24 12:03:39 -0600

    added theming from MUI to root

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { StateProvider } from './utils/StateProvider';
-import reducer, { initialState } from './utils/reducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/reducer";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-    <App />
-    </StateProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<StateProvider initialState={initialState} reducer={reducer}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<App />
+			</ThemeProvider>
+		</StateProvider>
+	</React.StrictMode>
 );
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	pallete: {
+		primary: {
+			main: "#2176FF",
+			secondary: "#33A1FD",
+		},
+	},
+});
+
+export default theme;
