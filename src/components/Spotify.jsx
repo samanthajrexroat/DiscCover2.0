f9a62e17a61edad422fc0b31fb126bab8239ee1c
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-05-25 14:13:30 -0600

    Added body and navbar components and imported to spotify jsx

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Footer from './Footer';
 import Sidebar from './Sidebar';
+import Body from './Body';
+import Navbar from './Navbar';
 
 export default function Spotify() {
   return (
@@ -8,9 +10,9 @@ export default function Spotify() {
         <div className="spotify_body">
             <Sidebar />
             <div className="body">
-                
+                <Navbar />
                 <div className="body_contents">
-                    
+                    <Body />
                 </div>
             </div>
         </div>
